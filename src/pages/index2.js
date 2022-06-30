62ac5e7a546c22f07352ab7374e982dcc678baa8
Stephen Parker <sparker@gravitaldigital.com>
2022-06-29 21:08:32 -0400

    new nextjs nav

---
+++ src/pages/index2.js
@@ -0,0 +1,11 @@
+import * as React from "react"
+import Navigation from "../components/navigation"
+import Secondarynav from "../components/secondarynav"
+
+export default function Index2() {
+  return (
+    <>
+      <Navigation />
+    </>
+  )
+}
